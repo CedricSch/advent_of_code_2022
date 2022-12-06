52b6070f908d26935855d4aa20499287430e631b
cedric <cedric.schaub3@gmail.com>
2022-12-06 10:08:49 +0100

    Add day 4 part one

---
+++ day_04/main.cpp
@@ -0,0 +1,65 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <tuple>
+#include <vector>
+
+using number_pair = std::tuple<unsigned int, unsigned int>;
+using section_pair = std::tuple<number_pair, number_pair>;
+
+// Format <number>-<number>,<number>-<number>
+// use stack to get the numbers
+// static section_pair get_section_pair2(std::string const& line) {
+//     // find , delimiter
+//     auto sep = line.find_first_of(',');
+//     std::string first_section_pair = line.substr(0, sep);
+//     std::string second_section_pair = line.substr(sep+1, line.size());
+//
+//     return {};
+// }
+
+// Format <number>-<number>,<number>-<number>
+static section_pair get_section_pair(std::string_view line) {
+  auto comma_pos = line.find_first_of(',');
+  auto first_seperator_pos = line.find_first_of('-');
+  auto second_seperator_pos = line.find_last_of('-');
+
+  auto first_number = line.substr(0, first_seperator_pos);
+  auto second_number = line.substr(first_seperator_pos + 1, comma_pos - first_seperator_pos - 1);
+  auto third_number = line.substr(comma_pos + 1, second_seperator_pos - comma_pos - 1);
+  auto fourth_number = line.substr(second_seperator_pos + 1, line.size());
+
+  number_pair pair_one = std::make_tuple(std::stoul(std::string(first_number)), std::stoul(std::string(second_number)));
+  number_pair pair_two = std::make_tuple(std::stoul(std::string(third_number)), std::stoul(std::string(fourth_number)));
+
+  return std::make_tuple(pair_one, pair_two);
+}
+
+static bool is_pair_fully_contained(number_pair const &pair_one, number_pair const &pair_two) {
+  return std::get<0>(pair_one) >= std::get<0>(pair_two) and std::get<1>(pair_one) <= std::get<1>(pair_two) or
+         std::get<0>(pair_two) >= std::get<0>(pair_one) and std::get<1>(pair_two) <= std::get<1>(pair_one);
+}
+
+int main() {
+  std::string line;
+  std::ifstream file("input.txt");
+
+  if (file.fail())
+    return 1;
+
+  std::vector<section_pair> section_pairs{};
+
+  while (std::getline(file, line)) {
+    section_pairs.push_back(get_section_pair(line));
+  }
+
+  std::size_t counter{0};
+  for (auto const &pairs : section_pairs) {
+    if (is_pair_fully_contained(std::get<0>(pairs), std::get<1>(pairs))) {
+      counter++;
+    }
+  }
+
+  std::cout << counter << " pairs are fully contained\n";
+}
