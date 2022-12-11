e371f71de672108a8e73e1b0fc2d73b183c11147
cedric <cedric.schaub3@gmail.com>
2022-12-11 15:39:13 +0100

    Add day 07 part two

--- day_07/main.cpp
+++ day_07/main.cpp
@@ -3,64 +3,70 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <memory>
+#include <tuple>
 
-struct Node {
-    enum class Type { FILE, DIRECTORY };
-    Type type = Type::DIRECTORY;
-    Node* parent = nullptr;
-    std::vector<Node*> childs;
+struct Directory {
+    Directory* parent = nullptr;
+    std::vector<Directory*> childs;
     std::string name;
     unsigned int size = 0;
-};
-
-struct Command {
-    enum class Type { CD, LS, NOC };
-    Type type = Type::NOC;
-    std::string arg;
-};
-
-static Node* get_node(std::string const& line) {
-    Node* node = new Node;
-    if(line.starts_with("dir")) {
-        node->name = line.substr(4);
-    } else {
-        node->type = Node::Type::FILE;
-        auto ws_pos = line.find_first_of(' ');
-        node->size = std::stoul(line.substr(0, ws_pos));
-        node->name = line.substr(ws_pos + 1);
+    ~Directory() {
+        for(auto ptr : childs) delete ptr;
+        childs.clear();
     }
+};
 
-    return node;
-}
+enum class CommandType { NOC, CD, LS, FILE, DIR };
 
-void print_filesystem(Node* const start, int indent, int& counter) {
-    if(start->childs.size() == 0) return;
-
+void recalculate_sizes(Directory* const start) {
     for(auto child : start->childs) {
-        print_filesystem(child, indent+1, counter);
+        recalculate_sizes(child);
         start->size += child->size;
     }
+}
 
-    std::string indent_str(indent, ' ');
-    if(start->type == Node::Type::DIRECTORY and start->size <= 100'000) {
-        counter += start->size;
-        std::cout << indent_str << "[Dir] " << start->name << ", size: " << start->size << " \n";
-    } else {
-        //std::cout << indent_str << "[File] " << start->name << ", size: " << start->size << " \n";
+static void part_one(Directory const* const start, unsigned int& part_one_sum) {
+    if(start->size <= 100'000) part_one_sum += start->size;
+
+    for(auto child : start->childs) {
+        part_one(child, part_one_sum);
     }
 }
 
-static Command get_command(std::string const& line) {
-    Command cmd;
-    if(line.starts_with("cd")) {
-        cmd.type = Command::Type::CD;
-        // cd<whitespace> = 3
-        cmd.arg = line.substr(3);
-    } else {
-        cmd.type = Command::Type::LS;
-    }
+static void part_two_rec(Directory const* const start, unsigned int unused_space, std::vector<unsigned int>& candidates) {
+    static const int necessary_space = 30'000'000;
 
-    return cmd;
+    if(start->size >= (necessary_space - unused_space)) candidates.push_back(start->size);
+
+    for(auto child : start->childs) {
+        part_two_rec(child, unused_space, candidates);
+    }
+}
+
+static void part_two(Directory const* const start, unsigned int currently_used_space) {
+    static const int available_space = 70'000'000;
+
+    unsigned int unused_space = available_space - currently_used_space;
+
+    std::vector<unsigned int> candidates;
+    // find a directory to delete
+    part_two_rec(start, unused_space, candidates);
+
+    auto min = std::min_element(candidates.begin(), candidates.end());
+    std::cout << "Min. element to free space: " << *min << std::endl;
+}
+
+using CommandTypeResult = std::tuple<CommandType, std::string>;
+
+static CommandTypeResult get_command(std::string const& line) {
+    if(line.starts_with("$ cd")) return std::make_tuple(CommandType::CD, line.substr(5));
+    if(line.starts_with("$ ls")) return std::make_tuple(CommandType::LS, "");
+    if(line.starts_with("dir ")) return std::make_tuple(CommandType::DIR, line.substr(4));
+    
+    // at this point it can only be a file
+    auto ws_pos = line.find_first_of(' ');
+    return std::make_tuple(CommandType::FILE, line.substr(0, ws_pos));
 }
 
 int main() {
@@ -70,40 +76,56 @@ int main() {
   if (file.fail())
     return 1;
 
-  Command last_command{};
-  Node* current_directory = new Node;
-  current_directory->name = "/";
-  Node* root = current_directory;
+  CommandTypeResult next_command = std::make_tuple(CommandType::NOC, "");
+  Directory* current_directory = new Directory({.name = "/"});
+  Directory* root = current_directory;
 
+  // ignore the first cd / line
   std::getline(file, line);
   while (std::getline(file, line)) {
-      // Command
-      if(line.starts_with('$')) {
-        last_command = get_command(line.substr(2));
-        //std::cout << "Last command: " << (last_command.type == Command::Type::CD ? "cd" : "ls") << " with arg: " << (last_command.type == Command::Type::LS ? "<none>" : last_command.arg) << std::endl;
-        if(last_command.type == Command::Type::LS) continue;
-      } 
-      
-      if(last_command.type == Command::Type::CD) {
-          if(last_command.arg == "..") {
-              std::cout << "===.. " << current_directory->name << " ---> " << current_directory->parent->name << " ===\n";
-              current_directory = current_directory->parent;
-          } else {
-              auto ptr = std::find_if(current_directory->childs.begin(), current_directory->childs.end(), [last_command](auto* ptr){ return last_command.arg == ptr->name and ptr->type == Node::Type::DIRECTORY; });
-              if(ptr == current_directory->childs.end()) {
-                  std::cout << "NULLPTR\n";
-              }
-              std::cout << "=== " << current_directory->name << " ---> " << (*ptr)->name << " ===\n";
-              current_directory = *ptr;
-          }
-      } else {
-          auto* node = get_node(line);
-          //std::cout << (node->type == Node::Type::FILE ? "File" : "Directory") << " with name: " << node->name << " and size: " << node->size << std::endl;
-          node->parent = current_directory;
-          current_directory->childs.push_back(node);
+      next_command = get_command(line);
+      auto command_type = std::get<0>(next_command);
+      std::string command_arg = std::get<1>(next_command);
+
+      // if we do a ls we only can work with the next line(s)
+      if(command_type == CommandType::LS) continue;
+
+      // go one directory down
+      if(command_type == CommandType::CD and command_arg == "..") {
+          current_directory = current_directory->parent;
+      } else if(command_type == CommandType::CD) {
+          auto next_dir = std::find_if(current_directory->childs.begin(), current_directory->childs.end(), [&command_arg](Directory* dir){ return dir->name == command_arg; }); 
+          if(next_dir == current_directory->childs.end()) std::cout << "nullptr\n";
+          current_directory = *next_dir;
       }
+
+      // create new child in the current directory
+      if(command_type == CommandType::DIR) {
+          auto dir = new Directory;
+          dir->parent = current_directory;
+          dir->name = command_arg;
+          current_directory->childs.push_back(dir); 
+      }
+
+      // only add file size to the current directory (files are not from interest in this exercise) 
+      if(command_type == CommandType::FILE) {
+          auto size = std::stoul(command_arg);
+          current_directory->size += size;
+      } 
+  }  
+
+  recalculate_sizes(root);
+
+  unsigned int part_one_sum{0};
+  part_one(root, part_one_sum);
+
+  std::cout << "Part one - total size of directories <= 100'000: " << part_one_sum << std::endl;
+
+  part_two(root, root->size);
+
+  for(auto ptr : root->childs) {
+      delete ptr;
   }
-  int counter{0};
-  print_filesystem(root, 0, counter);
-  std::cout << "Combined size: " << counter << std::endl;
+  delete root;
 }
+
